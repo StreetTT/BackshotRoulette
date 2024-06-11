b8ab2b21fe4c3b1228cb3e41aa3261dec04fdf43
Bow (Juno) <33423299+StreetTT@users.noreply.github.com>
2024-06-11 12:34:56 +0100

    Initially Receiving Client’s Actions
    Developed a system to handle client server communication when using items. Whilst not fully implemented the framework is there.
    Also implemented SocketBR.__RQRESProtocol as a wrapper to perform communication

--- SocketBR.py
+++ SocketBR.py
@@ -10,7 +10,6 @@ from time import sleep
 import sys
 
 
-
 DISCONNECTMSG = "!DISCONNECT"
 
 
@@ -20,6 +19,7 @@ class Router():
         self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__server.bind((self.__serverName, 5050))
         self.__games: dict[str, SocketBR] = {"ABCD": SocketBR("ABCD")}
+
         self.__clients: dict[str, str] = {}
         self.__lock = threading.Lock()
     
@@ -100,22 +100,30 @@ class SocketBR():
     
     def __StartGame(self):
         with self.__lock:
-            self.__SendToPlayersConcurrently(self.__SetName)
+            self.__SendToPlayersConcurrently(self.__SetName, direct=True)
         self.__name = f"{self.__players[0].GetName()} vs {self.__players[1].GetName()}"
-        print(self.__name)
+        print(self.__name + "\n-----")
         sys.stdout = DualOutput(datetime.now().strftime(f'log/{self.__name.replace(" ","_")}-%d_%b_%y_%Hh_%Mm_%Ss_%fms.log'))
         self.__SetRounds()
         for index, self.__currentRound in enumerate(self.__rounds):
-            print(f"Round {index + 1}")
+            print(f"Round {index + 1}" + "\n----")
             self.__StartRound()
             while not self.__IsRoundOver():
+                print("---")
+                for player in self.__players:
+                    print(player.Info())
+                print(self.__gun.Info())
                 self.__SendToPlayersConcurrently(self.__SendRoundInfo)
-
+                choice = self.__TakeAction()
+                if choice in [str(item.__name__)[2] for item in self.__items]:
+                    self.__UseItem(choice)
+                else:
+                    pass # impact = self.__ShootSomeone(int(choice))
 
     def __SetName(self, player: "Player"): # Request the name of each player
-        player.SendMessage("!!NAMERQ")  
+        player.SendMessage("!!RQ")  
         buffer = player.ReceiveMessage()
-        while buffer != "!!NAMERES":
+        while buffer != "!!RES":
             player.SetName(buffer)
             print(f"{self.__code}/{str(player)} - {buffer}")
             buffer = player.ReceiveMessage()
@@ -125,11 +133,15 @@ class SocketBR():
             self.__rounds = [Round(i+2,i) for i in range(0,6,2)]
         else:
             self.__rounds = [Round(randint(2,4),randint(1,4)) for _ in range(3)]
-            self.__items += [
-                self.__Twist,
-                self.__Spike,
-                self.__8Ball,
-                self.__Pluck
+            # self.__items += [
+            #     self.__Twist,
+            #     self.__Spike,
+            #     self.__8Ball,
+            #     self.__Pluck
+            # ]
+            self.__items = [
+                self.__Knife,
+                self.__Glass
             ]
     
     def __StartRound(self) -> None:
@@ -138,6 +150,7 @@ class SocketBR():
             player._GetGallery()._Clear()
         self.__currentPlayer = self.__players[0]
         self.__Reload()
+        print(self.__currentRound.Info())
     
     def __Reload(self):
         if not self.__gun._IsEmpty():
@@ -147,6 +160,7 @@ class SocketBR():
         for player in self.__players:
             items: int = self.__currentRound.GetItemsPerLoad()
             while items > 0 and not player._GetGallery()._IsFull():
+                # If Round Health == 2, no Knifes
                 player._GetGallery()._Add(choices(self.__items)[0])
                 items -= 1
     
@@ -164,59 +178,131 @@ class SocketBR():
         p = self.__currentPlayer if p == None else p
         return [player for player in self.__players if player != p][0]
     
-    def __SendToPlayersConcurrently(self, method, args=()):
+    def __SendToPlayersConcurrently(self, method, direct=False):
         threads = []
         for player in self.__players:
-            thread = threading.Thread(target=method, args=((player,)+args))
+            if direct: 
+                thread = threading.Thread(target=method, args=((player,)))
+            else:
+                thread = threading.Thread(target=self.__RQRESProtocol, args=(player, method, ))
             thread.start()
             threads.append(thread)
         for thread in threads:
             thread.join()
-
-    
+  
     def __SendRoundInfo(self, player: "Player"):
-        player.SendMessage("!!ROUNDINFORQ")
         player.SendMessage(self.__currentRound.Info())
         player.SendMessage(player.Info())
         player.SendMessage(self.__GetOpponent(player).Info())
         player.SendMessage(self.__gun.Info())
-        buffer = player.ReceiveMessage()
-        while buffer != "!!ROUNDINFORES":
-            print(f"{self.__code}/{str(player)} $ {player.GetName()} - {buffer}")
-            buffer = player.ReceiveMessage()
+
+    def __RQRESProtocol(self, player, preMethod = None, names=True):
+        buffer = []
+        player.SendMessage(f"!!RQ")
+        preMethod(player) if preMethod != None else None
+        buffer.append(player.ReceiveMessage())
+        while buffer[-1] != f"!!RES":
+            print(f"{self.__code}/{str(player)}{(' $ ' + player.GetName()) if names else ''} - {buffer[-1]}")
+            buffer.append(player.ReceiveMessage())
+        try:
+            return buffer[-2]
+        except IndexError:
+            return buffer[-1]
     
-    def __Knife(self):
-        pass  # Function body to be implemented
+    def __TakeAction(self, plucking=False):
+        player = self.__currentPlayer if not plucking else self.__GetOpponent()
+        options: list[str] = player._GetGallery()._Options() + (["0","1"] if not plucking else [])
+        while True:
+            try:
+                choice:str = self.__RQRESProtocol(player)
+                if choice not in options:
+                    raise ValueError
+                if choice == "P":
+                    if plucking:
+                        raise IndexError
+                    elif self.__GetOpponent()._GetGallery()._Options() == [] or all(option == "P" for option in self.__GetOpponent()._GetGallery()._Options()):
+                        raise LookupError
+                return choice
+            except ValueError:
+                print(f"{choice} is not an option. Please select a valid option {str(options)}")
+            except IndexError:
+                print(f"You cannot pluck a pluck when plucking. Please select another option")
+            except LookupError:
+                print(f"There isn't enough items to Pluck. Please select another option")
 
-    def __Glass(self):
-        pass  # Function body to be implemented
+    def __UseItem(self, choice:str, plucked=False):
+        player: Player = self.__currentPlayer if not plucked else self.__GetOpponent()
+        item: Callable | None = player._GetGallery()._Use(choice)
+        if item is not None:
+            if item.__name__[2:] != "Pluck":
+                pass
+            announcement = item()
+            print(announcement)
+            announcementMethod = lambda player: player.SendMessage(announcement)
+            if choice in ['G', '8']:
+                self.__RQRESProtocol(self.__currentPlayer,announcementMethod)
+            else:
+                self.__SendToPlayersConcurrently(announcementMethod)
 
-    def __Drugs(self):
-        pass  # Function body to be implemented
+    def __Knife(self) -> None:
+        self.__gun._SetCrit(True)   
+        announcement = "Gun = Crit"
+        return announcement
+        
+    def __Glass(self) -> None:
+        announcement = "Top Bullet = "
+        if self.__gun._Peek():
+            announcement += "LIVE"
+        else:
+            announcement += "DEAD"
+        return announcement
 
-    def __Cuffs(self):
-        pass  # Function body to be implemented
+    def __Drugs(self) -> None:
+        self.__currentPlayer._ModifyHealth(1, self.__rounds[self.__currentRound]['MaxHealth'])
+        print(self.__currentPlayer._GetName() + "'s health = " + str(self.__currentPlayer._GetHealth()))
 
-    def __Voddy(self):
-        pass  # Function body to be implemented
+    def __Cuffs(self) -> None:
+        self.__GetOpponent()._SetCuffed(True)
+        print(self.__GetOpponent()._GetName() + " has been handcuffed")
 
-    def __Twist(self):
-        pass  # Function body to be implemented
-
-    def __Spike(self):
-        pass  # Function body to be implemented
-
-    def __8Ball(self):
-        pass  # Function body to be implemented
+    def __Voddy(self) -> None:
+        print("The bullet was", end=": ")
+        if self.__gun._Rack():
+            print("LIVE")
+        else:
+            print("DEAD")
+    
+    def __Twist(self) -> None:
+        print("The bullet has been Twisted")
+        self.__gun._Twist()
+    
+    def __Spike(self) -> None:
+        spike = 2 if randint(0, 99) < 40 else -1
+        self.__currentPlayer._ModifyHealth(spike, self.__rounds[self.__currentRound]['MaxHealth'])
+        print(self.__currentPlayer._GetName() + "'s health = " + str(self.__currentPlayer._GetHealth()))
+    
+    def __8Ball(self) -> None:
+        bullet = self.__gun._RandPeek()
+        print(f"Bullet {bullet[0]} is", end=": ")
+        if bullet[1]:
+            print("LIVE")
+        else:
+            print("DEAD")
 
     def __Pluck(self):
-        pass  # Function body to be implemented
+        return  # Function body to be implemented
+        choice = self.__ActionMenu(plucking=True)
+        print("-")
+        actionData, reward = self.__UseItem(choice, plucked=True)
+        return (actionData, reward)
 
 
 class Player(BRProtocol):
     def __init__(self, connection, address) -> None:
         self.__address = address
         self._connection = connection
+        self.__name = None
+        self.__health = 0
         self.__wins = 0
         self.__gallery: Gallery = Gallery()
     
@@ -243,6 +329,7 @@ class Player(BRProtocol):
     
     def Info(self):
         d = {}
+        d.update({"name": self.__name}) if self.__name != None else None
         d.update({"wins": self.__wins})
         d.update({"gallery": str(self.__gallery)})
         d.update({"health": self.__health})
@@ -277,7 +364,6 @@ class Gun:
             return True
         return False
         
-
     def __str__(self) -> None:
         m:str = "     ---- "
         for bullet in self.__chamber:
@@ -309,10 +395,6 @@ class Gun:
         d.update({"crit": self.__crit})
         return d
     
-    
-    
-    
-
     def __repr__ (self) -> str:
         return str(self.__chamber)
     
@@ -322,6 +404,8 @@ class Gun:
     def _GetCrit(self) -> bool:
         return self.__crit
 
+    def _Peek(self) -> bool:
+        return self.__chamber[0]
 
 class Gallery():
     def __init__(self) -> None:
@@ -343,6 +427,16 @@ class Gallery():
         if len(self.__items) == 8:
             return True
         return False
+
+    def _Options(self) -> list[str]:
+        return [item[0] for item in eval(str(self))]
+    
+    def _Use(self, choice:str) -> Callable | None:
+        try:
+            index: int = self._Options().index(choice)
+        except ValueError:
+            return None
+        return self.__items.pop(index)
 
 
 
